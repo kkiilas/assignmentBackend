3086985ca1717b123eddfe13c1182ad69d5b8250
kkiilas <kadri.kiilas@helsinki.fi>
2022-05-26 20:07:23 +0300

    uibuild

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.get('/', async (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
